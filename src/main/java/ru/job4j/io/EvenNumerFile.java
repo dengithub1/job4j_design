1e6ede4964452c3fd9b68e1c2362b411fc0132e9
admin <desantt8@gmail.com>
2025-08-01 15:18:08 +0300

    2.1. FileInputStream [#4898] modified fix ci bot errors

--- src/main/java/ru/job4j/io/EvenNumerFile.java
+++ src/main/java/ru/job4j/io/EvenNumerFile.java
@@ -2,7 +2,6 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class EvenNumerFile {
     public static void main(String[] args) {
