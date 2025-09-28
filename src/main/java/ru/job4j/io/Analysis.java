4b92b87cb73439d89b247a497888c084a99593b5
admin <desantt8@gmail.com>
2025-09-28 18:42:08 +0300

    7. Анализ доступности сервера [#859 #552457] refactored method

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,39 +1,30 @@
 package ru.job4j.io;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Analysis {
 
     public void unavailable(String source, String target) {
-        ArrayList<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(target)) {
             String line;
+            boolean isUnavailable = false;
+            String startTime = null;
             while ((line = reader.readLine()) != null) {
-                list.add(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ArrayList<String> availableList = new ArrayList<>();
-        boolean isUnavailable = false;
-        String startTime = null;
-        for (String line : list) {
-            String[] parts = line.split(" ");
-            String status = parts[0];
-            String time = parts[1];
-            if ((status.equals("400") || status.equals("500")) && !isUnavailable) {
-                startTime = time;
-                isUnavailable = true;
-            } else if (isUnavailable) {
-                if (!(status.equals("400") || status.equals("500"))) {
-                    availableList.add(startTime + ";" + time + ";");
+                String[] parts = line.split(" ");
+                String status = parts[0];
+                String time = parts[1];
+                if ((status.equals("400") || status.equals("500")) && !isUnavailable) {
+                    startTime = time;
+                    isUnavailable = true;
+                } else if (isUnavailable) {
+                    if (!(status.equals("400") || status.equals("500"))) {
+                        writer.println(startTime + ";" + time + ";");
+                    }
                 }
-            }
-        }
-        try (PrintWriter writer = new PrintWriter(target)) {
-            for (String time : availableList) {
-                writer.println(time);
             }
         } catch (IOException e) {
             e.printStackTrace();
