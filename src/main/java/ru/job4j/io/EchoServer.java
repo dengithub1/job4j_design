41fb51285bee9ce06fe444a955db44228998107b
admin <desantt8@gmail.com>
2025-10-07 09:40:23 +0300

    3. Бот [#7921 #552830] deleted 'for'

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,6 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
